a163ce745f4fbce8faa1873b41b3799244654d05
nsankaran <nsankaran@ebay.com>
2014-09-14 11:04:27 -0700

    instagram client is working

---
+++ src/com/github/snambi/instagramviewer/InstagramPhoto.java
@@ -0,0 +1,49 @@
+package com.github.snambi.instagramviewer;
+
+public class InstagramPhoto {
+	
+	private String username;
+	private String imageUrl;
+	private int imageHeight;
+	private String caption;
+	private int likesCount;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	public int getImageHeight() {
+		return imageHeight;
+	}
+	public void setImageHeight(int imageHeight) {
+		this.imageHeight = imageHeight;
+	}
+	public String getCaption() {
+		return caption;
+	}
+	public void setCaption(String caption) {
+		this.caption = caption;
+	}
+	public int getLikesCount() {
+		return likesCount;
+	}
+	public void setLikesCount(int likesCount) {
+		this.likesCount = likesCount;
+	}
+	
+	@Override
+	public String toString() {
+		return "InstagramPhoto [username=" + username + ", imageUrl="
+				+ imageUrl + ", imageHeight=" + imageHeight + ", caption="
+				+ caption + ", likesCount=" + likesCount + "]";
+	}
+
+}
--- src/com/github/snambi/instagramviewer/PhotosActivity.java
+++ src/com/github/snambi/instagramviewer/PhotosActivity.java
@@ -1,21 +1,84 @@
 package com.github.snambi.instagramviewer;
 
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
 
 public class PhotosActivity extends Activity {
+	
+	public static final String INSTAGRAM_CLIENT_ID = "2bd2c98f31cd4a6b94f2e5e6231680e9";
+	public static final String INSTAGRAM_POPULAR_PHOTOS_EP = "https://api.instagram.com/v1/media/popular?client_id=" + INSTAGRAM_CLIENT_ID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photos);
+        
+        fetchPopularPhotos();
     }
 
 
-    @Override
+    private void fetchPopularPhotos() {
+		// create async client
+    	AsyncHttpClient httpClient = new AsyncHttpClient();
+    	
+    	// make a network call
+    	httpClient.get(INSTAGRAM_POPULAR_PHOTOS_EP, new JsonHttpResponseHandler(){
+    		
+    		// handle successful responses
+    		@Override
+    		public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+    			
+    			if( response !=null ){
+    				JSONArray photosJSON = null;
+    				
+    				try {
+						photosJSON = response.getJSONArray("data");
+						
+						for( int i=0; i < photosJSON.length() ; i++){
+							
+							JSONObject photoJson = photosJSON.getJSONObject(i);
+							
+							InstagramPhoto photo = new InstagramPhoto();
+							
+							photo.setUsername(photoJson.getJSONObject("user").getString("username"));
+							photo.setImageUrl( photoJson.getJSONObject("images").getJSONObject("standard_resolution").getString("url"));
+							photo.setImageHeight( photoJson.getJSONObject("images").getJSONObject("standard_resolution").getInt("height"));
+							photo.setCaption( photoJson.getJSONObject("caption").getString("text"));
+							photo.setLikesCount(photoJson.getJSONObject("likes").getInt("count"));
+							
+							Log.i("INFO", photo.toString());
+						}
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+    			}
+    		}
+    		
+    		// handle failures
+    		@Override
+    		public void onFailure(int statusCode, Header[] headers,
+    				Throwable throwable, JSONObject errorResponse) {
+    			
+    			super.onFailure(statusCode, headers, throwable, errorResponse);
+    		}
+    	});
+		
+	}
+
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.photos, menu);
